79e652a58a7683af9b66e72b082b2cde194c71d5
rajnish432 <rajnish.kahar1996@gmail.com>
2020-08-13 19:20:33 +0530

    [Rajnish]ADDED: Test Case To Perform Null Check

--- QuantityMeasurement/QuantityMeasurement.cs
+++ QuantityMeasurement/QuantityMeasurement.cs
@@ -1,10 +1,45 @@
-﻿namespace QuantityMeasurement
+﻿// <copyright file="QuantityMeasurement.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace QuantityMeasurement
 {
+    /// <summary>
+    /// Main Class To Perform Conversion.
+    /// </summary>
     public class QuantityMeasurement
     {
+        /// <summary>
+        /// Function to get double values.
+        /// </summary>
+        /// <param name="feet"></param>
+        /// <returns></returns>
         public double GetFeet(double feet)
         {
             return feet;
+        }
+
+        /// <summary>
+        /// Overrriding Equals Method.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Overriding GetHashCode Method.
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
         }
     }
 }
--- QuantityMeasurementTest/QuantityMeasurementTest.cs
+++ QuantityMeasurementTest/QuantityMeasurementTest.cs
@@ -1,31 +1,59 @@
-using NUnit.Framework;
-
+// <copyright file="QuantityMeasurementTest.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 namespace QuantityMeasurementTest
 {
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Class to Test QuantityMeasurement.
+    /// </summary>
     public class QuantityMeasurementTest
     {
-        QuantityMeasurement.QuantityMeasurement quantityMeasurement;
+        /// <summary>
+        /// Calling Main Class.
+        /// </summary>
+        private QuantityMeasurement.QuantityMeasurement quantityMeasurement;
 
+        /// <summary>
+        /// Setup Method.
+        /// </summary>
         [SetUp]
         public void Setup()
         {
-            quantityMeasurement = new QuantityMeasurement.QuantityMeasurement();
+            this.quantityMeasurement = new QuantityMeasurement.QuantityMeasurement();
         }
 
+        /// <summary>
+        /// Test 1.1: To Check 0 feet equals 0 feet.
+        /// </summary>
         [Test]
         public void GivenTwoFeetSameValues_WhenCompared_ShouldReturnEqual()
         {
-            double feet1 = quantityMeasurement.GetFeet(0.0);
-            double feet2 = quantityMeasurement.GetFeet(0.0);
-            Assert.AreEqual(feet1,feet2);
+            double feet1 = this.quantityMeasurement.GetFeet(0.0);
+            double feet2 = this.quantityMeasurement.GetFeet(0.0);
+            Assert.AreEqual(feet1, feet2);
         }
 
+        /// <summary>
+        /// Test 1.1: To Check 10 feet is not equal to 0 feet.
+        /// </summary>
         [Test]
         public void GivenTwoDifferentFeetValues_WhenCompared_ShouldReturnNotEqual()
         {
-            double feet1 = quantityMeasurement.GetFeet(0.0);
-            double feet2 = quantityMeasurement.GetFeet(10.0);
+            double feet1 = this.quantityMeasurement.GetFeet(0.0);
+            double feet2 = this.quantityMeasurement.GetFeet(10.0);
             Assert.AreNotEqual(feet1, feet2);
+        }
+
+        /// <summary>
+        /// Test 1.2: To Check is Object is null.
+        /// </summary>
+        [Test]
+        public void GivenNullFeetValues_WhenChecked_ShouldReturnFalse()
+        {
+            bool result = this.quantityMeasurement.Equals(null);
+            Assert.IsFalse(result);
         }
     }
 }
